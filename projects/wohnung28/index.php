102af6636647ba4548b80b3c6fc884a651b8082f
ivrag <info@ivrag.ch>
2021-03-04 09:51:41 +0100

    projects/wohnung28 finishup

--- projects/wohnung28/index.php
+++ projects/wohnung28/index.php
@@ -34,7 +34,7 @@
     <?php new Header($cd) ?>
 
     <main class="container mt-5">
-        <div class="mb-3">
+        <div>
             <h2>Willkommen bei Wohnung 28</h2>
             <h5>Möblierte Ferienwohnungen für Freizeit und Business</h5>
         </div>
@@ -49,76 +49,99 @@
                 </div>
             </div>
             <div class="col-md-6 pt-1">
-                <p>Wohnung 28 ist ein Projekt der Firma Immobilien Von Rehetobel AG welches von einem jungen Team betrieben wird. Das Konzept hinter dem Projekt ist es Möblierte Wohnungen für Familien und Geschäftsreisende zur Verfügung zu stellen.</p>
+                <p>Wohnung 28 ist ein Projekt der Firma Immobilien Von Rehetobel AG welches von einem jungen Team betrieben wird. Das Konzept hinter dem Projekt ist es möblierte Wohnungen für Familien und Geschäftsreisende zur Verfügung zu stellen.</p>
                 <p>Durch die modernen und stylischen Einrichtungen unserer Wohnungen wird eine angenehme Atmosphäre geschafft. Alle Einrichtungen wurden mit viel Liebe zum Detail designt. Neben den Einrichtungen gibt es verschiedene Raumaccessoires, die Ihren Aufenthalt noch angenehmer gestalten.</p>
                 <p>Mit dem Projekt Wohnung 28 möchten wir unseren Kunden einen unvergesslichen Aufenthalt bieten, den Sie nie wieder vergessen werden.</p>
                 <p>Die staunenden Blicke und positive Rückmeldungen unserer Gäste motivieren uns, aus diesem Projekt etwas einzigartiges zu erschaffen.</p>
             </div>
         </div>
 
-        <div class="mt-5 mb-3">
-            <h2>Wohnung 29</h2>
-            <h5>Exklusive 4,5 Zimmer Wohnung für Familien und Business</h5>
-        </div>
-        <div class="row">
+        <div class="row textsection-space">
             <div class="col-md-6 pt-1">
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium illo deserunt velit vitae ab, qui placeat modi accusamus cum labore? Magni dignissimos facilis voluptate doloribus dolor minima nam nostrum modi?</p>
-            </div>
-            <div class="col-md-6 pt-2">
-                <div id="wg-29-interval" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="../../lib/images/flats/29/flat_10.png" class="d-block w-100" alt="...">
+                <div id="wg-28-interval" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#wg-28-interval" data-slide-to="0" class="active"></li>
+                        <li data-target="#wg-28-interval" data-slide-to="1"></li>
+                        <li data-target="#wg-28-interval" data-slide-to="2"></li>
+                    </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../../lib/images/flats/28/flat_1.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../lib/images/flats/28/flat_2.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../lib/images/flats/28/flat_3.png" class="d-block w-100" alt="...">
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img src="../../lib/images/flats/29/flat_16.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../../lib/images/flats/29/flat_18.png" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#wg-29-interval" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#wg-29-interval" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                        <a class="carousel-control-prev" href="#wg-28-interval" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#wg-28-interval" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                 </div>
+            </div>
+            <div class="col-md-6">
+                <div>
+                    <h2>Wohnung 28</h2>
+                    <h5>Exklusive 5,5 Zimmer Wohnung für Familien und Business</h5>
+                </div>
+                <p>Die 5,5 Zimmer Wohnung mit 161 m<sup>2</sup> wurde im Juni 2020 komplett eingerichtet und bietet Platz für 10 Personen.</p>
+                <p>Das absolute Highlight dieser Wohnung ist, dass Sie mit viel Liebe zum Detail eingerichtet wurde. Die Einrichtung strahlt eine afrikanische Thematik ab und es scheint, als wäre man weit weg von der Heimat in einer Wohnung nahe der afrikanischen Steppen.</p>
+                <p>Durch die 2 Lichtschächte im Gang und der Glasfront im Wohnzimmer erhält die Wohnung natürliches Licht von aussen und bietet somit mehr als genug Helligkeit.</p>
+                <p>Kunstwerke, Vasen und Bilder zieren die Wände und Räumlichkeiten. In den Schlafzimmern sorgen Kleiderdiener und Kleiderschränke für einen organisierten Start in den Tag.</p>
+                <p>Haben wir Ihr Interesse geweckt?</p>
+                <div><a href="https://www.airbnb.ch/rooms/43182277" class="btn btn-primary btn-md btn-block font-weight-bold" role="button" rel="noopener noreferrer" target="_blank">Besuchen Sie die Wohnung 28 jetzt auf Airbnb!</a></div>
             </div>
         </div>
 
-        <div class="mt-5 mb-3">
-            <h2>Wohnung 28</h2>
-            <h5>Exklusive 5,5 Zimmer Wohnung für Familien und Business</h5>
-        </div>
-        <div class="row">
-            <div class="col-md-6 pt-2">
-                <div id="wg-28-interval" class="carousel slide" data-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="carousel-item active">
-                            <img src="../../lib/images/flats/28/flat_1.png" class="d-block w-100" alt="...">
+        <div class="row textsection-space mb-5">
+            <div class="col-md-6 pt-1">
+                <div id="wg-29-interval" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#wg-29-interval" data-slide-to="0" class="active"></li>
+                        <li data-target="#wg-29-interval" data-slide-to="1"></li>
+                        <li data-target="#wg-29-interval" data-slide-to="2"></li>
+                        <li data-target="#wg-29-interval" data-slide-to="3"></li>
+                    </ol>
+                        <div class="carousel-inner">
+                            <div class="carousel-item active">
+                                <img src="../../lib/images/flats/29/flat_10.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../lib/images/flats/29/flat_16.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../lib/images/flats/29/flat_18.png" class="d-block w-100" alt="...">
+                            </div>
+                            <div class="carousel-item">
+                                <img src="../../lib/images/flats/29/flat_5.png" class="d-block w-100" alt="...">
+                            </div>
                         </div>
-                        <div class="carousel-item">
-                            <img src="../../lib/images/flats/28/flat_2.png" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                            <img src="../../lib/images/flats/28/flat_3.png" class="d-block w-100" alt="...">
-                        </div>
-                    </div>
-                    <a class="carousel-control-prev" href="#wg-28-interval" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#wg-28-interval" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
+                        <a class="carousel-control-prev" href="#wg-29-interval" role="button" data-slide="prev">
+                            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Previous</span>
+                        </a>
+                        <a class="carousel-control-next" href="#wg-29-interval" role="button" data-slide="next">
+                            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                            <span class="sr-only">Next</span>
+                        </a>
                 </div>
             </div>
-            <div class="col-md-6 pt-1">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error, alias ipsam atque mollitia corporis dolorem facilis placeat magni unde veniam nobis, reiciendis obcaecati, nihil similique commodi quisquam vel esse velit.</p>
+            <div class="col-md-6">
+                <div>
+                    <h2>Wohnung 29</h2>
+                    <h5>Exklusive 4,5 Zimmer Wohnung für Familien und Business</h5>
+                </div>
+                <p>Die 4,5 Zimmer Wohnung mit rund 115 m<sup>2</sup> wurde im September 2020 komplett eingerichtet. Hochwertige Möbel und dekorative Details zieren den Innenraum für einen wohltuhenden Aufenthalt.</p>
+                <p>Der grosszügige Balkon mit Blick auf die Burg Hohenklingen ist perfekt für ein entspanntes Glas Rotwein mit den Liebsten oder eine Tasse Kaffee in morgendlicher Atmosphäre.</p>
+                <p>Dank dem grosszugigen Raumverhältnis bietet die Wohnung Platz für bis zu 8 Personen.</p>
+                <p>Haben wir Ihr Interesse geweckt?</p>
+                <div><a href="https://www.airbnb.ch/rooms/45487943" class="btn btn-primary btn-md btn-block font-weight-bold" role="button" rel="noopener noreferrer" target="_blank">Besuchen Sie die Wohnung 29 jetzt auf Airbnb!</a></div>
             </div>
         </div>
     </main>
