439c2b279ec8a41624fcbdf4f52aa0cf553995e5
ivrag <info@ivrag.ch>
2021-03-10 10:36:22 +0100

    homepage setup

--- home/index.php
+++ home/index.php
@@ -35,51 +35,20 @@
 
     <main class="container mt-5">
         <div class="mb-5">
-            <h2>Home</h2>
-            <h5>Möblierte Ferienwohnungen für Freizeit und Business</h5>
+            <h2>Herzlich willkommen bei <i>Immobilien Von Rehetobel AG</i></h2>
         </div>
         <div class="row">
             <div class="col-md-6 pt-2">
                 <div id="carouselExampleInterval" class="carousel slide" data-ride="carousel">
                     <div class="carousel-inner">
                         <div class="carousel-item active">
-                        <img src="https://www.wohnung28.ch/src/images/drone/md/drone_1.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="https://www.wohnung28.ch/src/images/flat/md/flat_16.jpg" class="d-block w-100" alt="...">
-                        </div>
-                        <div class="carousel-item">
-                        <img src="https://www.wohnung28.ch/src/images/drone/md/drone_1.jpg" class="d-block w-100" alt="...">
+                            <img src="../lib/images/drone/drone.png" class="d-block w-100" alt="...">
                         </div>
                     </div>
-                    <a class="carousel-control-prev" href="#carouselExampleInterval" role="button" data-slide="prev">
-                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Previous</span>
-                    </a>
-                    <a class="carousel-control-next" href="#carouselExampleInterval" role="button" data-slide="next">
-                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                        <span class="sr-only">Next</span>
-                    </a>
                 </div>
             </div>
             <div class="col-md-6 pt-1">
-                <p>Die Wohnung 28 ist ein Projekt der Firma Immobilien Von Rehetobel AG welches von einem jungen Team betrieben wird. Das Konzept hinter dem Projekt ist es Möblierte Wohnungen für Familien und Geschäftsreisende zur Verfügung zu stellen.</p>
-                <p>Durch die modernen und stylischen Einrichtungen unserer Wohnungen wird eine angenehme Atmosphäre geschafft. Alle Einrichtungen wurden mit viel Liebe zum Detail designt. Neben den Einrichtungen gibt es verschiedene Raumaccessoires, die Ihren Aufenthalt noch angenehmer gestalten.</p>
-                <p>Mit dem Projekt Wohnung 28 möchten wir unseren Kunden einen unvergesslichen Aufenthalt bieten, den Sie nie wieder vergessen werden.</p>
-                <p>Die staunenden Blicke und positive Rückmeldungen unserer Gäste motivieren uns, aus diesem Projekt etwas einzigartiges zu erschaffen.</p>
-            </div>
-        </div>
-
-        <div class="mt-5 mb-3">
-            <h2>Frisch eingerichtete Wohnung</h2>
-            <h5>4.5 Zimmer Wohnung mit neuwertiger Möblierung</h5>
-        </div>
-        <div class="row">
-            <div class="col-md-6 pt-1">
-                <p>Gerne präsentieren wir Ihnen die neu eingerichtete 4.5 Zimmer Ferienwohnung, die in einem klassisch modernen Stil eingerichtet wurde und nun bereit ist Gäste zu empfangen.</p>
-            </div>
-            <div class="col-md-6 pt-2">
-                <img src="../lib/images/placeholder/placeholder.svg" class="d-block w-100" alt="...">
+                <p>Die Immobilien Von Rehetobel AG ist Ihre Adresse für </p>
             </div>
         </div>
     </main>
