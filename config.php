d19c44ca7763d7bf431b5bcc6453dcefd38a15c2
ivrag <info@ivrag.ch>
2021-04-28 16:25:15 +0200

    update config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@
 define("ROOT", realpath(dirname(__FILE__)) . "/");
 
 // --- DEVELOPMENT ---
-define("ROOT_DIR", "http://localhost/ivrag-admin/");
+define("ROOT_DIR", "http://localhost/ivrag-main/");
 
 // --- PRODUCTION ---
 // define("ROOT_DIR", "https://www.ivrag.ch/");
